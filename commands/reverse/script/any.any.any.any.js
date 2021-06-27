3cf59af8a85d6421519a650eb86700cf2b7b7f97
Pouya <pouya.eghbali@yandex.com>
2021-06-27 13:11:25 +0200

    Make reverse command read from stdin

--- commands/reverse/script/any.any.any.any.js
+++ commands/reverse/script/any.any.any.any.js
@@ -1,3 +1,11 @@
 module.exports.run = (argv) => {
-  console.log(argv.join(" ").split("").reverse().join(""));
+  if (argv.length) {
+    return console.log(argv.join(" ").split("").reverse().join(""));
+  }
+  const chunks = [];
+  process.stdin.on("data", (chunk) => chunks.push(chunk.toString()));
+  process.stdin.on("drain", () => {
+    console.log(chunks.join("").split("").reverse().join(""));
+    chunks.length = 0;
+  });
 };
