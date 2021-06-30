5739d2884ba70a2c624cad77f71c663a7ba2204e
Pouya <pouya.eghbali@yandex.com>
2021-06-30 19:04:04 +0200

    Added docker install instructions for ubuntu

---
+++ commands/docker/install/linux.ubuntu.any.x64.js
@@ -0,0 +1,25 @@
+const { execSync } = require("child_process");
+const ubuntu = require("@please.dev/lib/os/managers/ubuntu");
+
+const prerequisites = [
+  "apt-transport-https",
+  "ca-certificates",
+  "curl",
+  "gnupg",
+  "lsb-release",
+];
+
+const docker = ["docker-ce", "docker-ce-cli", "containerd.io"];
+
+const gpg = `curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg`;
+const addRepo = `echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null`;
+
+module.exports.install = (config) => {
+  ubuntu.repo.update();
+  ubuntu.install(prerequisites);
+  const stdio = config.silentInstall ? "ignore" : "pipe";
+  execSync(gpg, { stdio, shell: true });
+  execSync(addRepo, { stdio, shell: true });
+  ubuntu.repo.update();
+  ubuntu.install(docker);
+};
